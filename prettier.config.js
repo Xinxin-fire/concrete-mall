1fbbc281ea04c860268f0788e0ff9161ef5d7bc5
Yule <13166835035@163.com>
2022-06-15 15:53:23 +0800

    项目初始化

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  printWidth: 120, // 行宽
+  tabWidth: 2, // tab 空格数
+  useTabs: false,
+  semi: true, // 使用分号
+  vueIndentScriptAndStyle: true,
+  singleQuote: true, // 单引号
+  quoteProps: 'as-needed', // 属性引号
+  bracketSpacing: true, // 花括号空格
+  trailingComma: 'none', // 尾部逗号
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  arrowParens: 'always', // (x) => x + 1
+  insertPragma: false,
+  requirePragma: false,
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
+  rangeStart: 0
+};
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,15 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { shallowMount } from '@vue/test-utils';
+import HelloWorld from '@/components/HelloWorld.vue';
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
+// eslint-disable-next-line no-undef
+describe('HelloWorld.vue', () => {
+  // eslint-disable-next-line no-undef
+  it('renders props.msg when passed', () => {
+    const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+      propsData: { msg }
     });
+    // eslint-disable-next-line no-undef
     expect(wrapper.text()).toMatch(msg);
   });
 });
